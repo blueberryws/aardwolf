3ae56aa46ffccfff0f8d6bedd805ad81ec072b52
cody <cody@dadbox>
2025-04-18 15:14:08 -0600

    Anti-HTML paste

--- cdn/scripts/editors/text_editor.js
+++ cdn/scripts/editors/text_editor.js
@@ -5,6 +5,16 @@ export class TextEditor extends ElementEditor {
         super();
         this.element = element;
         this.element.addEventListener("focusout", () => this.clean());
+        this.element.addEventListener("paste", function(e) {
+            // cancel paste
+            e.preventDefault();
+        
+            // get text representation of clipboard
+            var text = (e.originalEvent || e).clipboardData.getData('text/plain');
+        
+            // insert text manually
+            document.execCommand("insertHTML", false, text);
+        });
     }
   getHTMLContent() { // startfold
     this.clean();
